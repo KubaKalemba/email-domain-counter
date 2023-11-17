412c0b663f5ef36396eff99c38b7fd38169c6cc4
Kuba <kuba.kalemba@gmail.com>
2023-11-17 15:11:28 +0100

    created separate package for csv handler

---
+++ csvhandler/csvhandler.go
@@ -0,0 +1,78 @@
+package csvhandler
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"sort"
+	"strings"
+)
+
+type KeyValue struct {
+	Key   string
+	Value int
+}
+
+func ReadAndCountDomains(filePath string) []KeyValue {
+	records := ReadCsvFile(filePath)
+
+	domainCounts, err := countEmailDomains(records)
+	if err != nil {
+		return nil
+	}
+
+	return domainCounts
+
+}
+
+func ReadCsvFile(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+
+	return records
+}
+
+func countEmailDomains(records [][]string) ([]KeyValue, error) {
+
+	emailDomainCount := make(map[string]int)
+
+	for _, record := range records {
+		if len(record) > 1 {
+			email := record[2]
+			domain := extractEmailDomain(email)
+			emailDomainCount[domain]++
+		}
+	}
+
+	return sortMapByDomain(emailDomainCount), nil
+}
+
+func extractEmailDomain(email string) string {
+	parts := strings.Split(email, "@")
+	if len(parts) == 2 {
+		return parts[1]
+	}
+	return ""
+}
+
+func sortMapByDomain(inputMap map[string]int) []KeyValue {
+	var domainCounts []KeyValue
+	for key, value := range inputMap {
+		domainCounts = append(domainCounts, KeyValue{key, value})
+	}
+
+	sort.Slice(domainCounts, func(i, j int) bool {
+		return domainCounts[i].Value > domainCounts[j].Value
+	})
+
+	return domainCounts
+}
--- handler.go
+++ handler.go
@@ -1,71 +1,12 @@
 package main
 
 import (
-	"encoding/csv"
+	"awesomeProject/csvhandler"
+	_ "awesomeProject/csvhandler"
 	"fmt"
 	"log"
-	"os"
 	"path/filepath"
-	"sort"
-	"strings"
 )
-
-type KeyValue struct {
-	Key   string
-	Value int
-}
-
-func readCsvFile(filePath string) [][]string {
-	f, err := os.Open(filePath)
-	if err != nil {
-		log.Fatal("Unable to read input file "+filePath, err)
-	}
-	defer f.Close()
-
-	csvReader := csv.NewReader(f)
-	records, err := csvReader.ReadAll()
-	if err != nil {
-		log.Fatal("Unable to parse file as CSV for "+filePath, err)
-	}
-
-	return records
-}
-
-func countEmailDomains(records [][]string) ([]KeyValue, error) {
-
-	emailDomainCount := make(map[string]int)
-
-	for _, record := range records {
-		if len(record) > 1 {
-			email := record[2]
-			domain := extractEmailDomain(email)
-			emailDomainCount[domain]++
-		}
-	}
-
-	return sortMapByDomain(emailDomainCount), nil
-}
-
-func extractEmailDomain(email string) string {
-	parts := strings.Split(email, "@")
-	if len(parts) == 2 {
-		return parts[1]
-	}
-	return ""
-}
-
-func sortMapByDomain(inputMap map[string]int) []KeyValue {
-	var domainCounts []KeyValue
-	for key, value := range inputMap {
-		domainCounts = append(domainCounts, KeyValue{key, value})
-	}
-
-	sort.Slice(domainCounts, func(i, j int) bool {
-		return domainCounts[i].Value > domainCounts[j].Value
-	})
-
-	return domainCounts
-}
 
 func main() {
 
@@ -76,11 +17,6 @@ func main() {
 
 	dataFilePath := filepath.Join(currentDir, "data.csv")
 
-	records := readCsvFile(dataFilePath)
-
-	domains, err := countEmailDomains(records)
-	if err != nil {
-		log.Fatal(err)
-	}
+	domains := csvhandler.ReadAndCountDomains(dataFilePath)
 	fmt.Println(domains)
 }
